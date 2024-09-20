961418378d18ed32ba6a56e0b23f9d1dbac5df46
aliNassef <anassef798@gmail.com>
2024-09-20 23:57:49 +0300

    add sentry to projecct error tracking

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'core/cache/cache_helper.dart';
 import 'core/di/dependency_injuction.dart';
 import 'taswaq.dart';
 import 'core/shared/widgets/custom_bloc_observer.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,10 +19,25 @@ void main() async {
   await CacheHelper().init();
   Bloc.observer = CustomBlocObserver();
   await setupGetIt();
-  runApp(
-    DevicePreview(
-      enabled: !kReleaseMode,
-      builder: (context) => const Taswaq(),
-    ),
-  );
+  if (kReleaseMode) {
+    await SentryFlutter.init(
+      (options) {
+        options.dsn =
+            'https://e82bc4ce56785673fcb7f155c27b21b9@o4507987094929408.ingest.us.sentry.io/4507987097288704';
+        // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+        // We recommend adjusting this value in production.
+        options.tracesSampleRate = .01;
+      },
+      appRunner: () => runApp(
+        const Taswaq(),
+      ),
+    );
+  } else {
+    runApp(
+      DevicePreview(
+        enabled: !kReleaseMode,
+        builder: (context) => const Taswaq(),
+      ),
+    );
+  }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,9 @@ import cloud_firestore
 import firebase_auth
 import firebase_core
 import google_sign_in_ios
+import package_info_plus
 import path_provider_foundation
+import sentry_flutter
 import shared_preferences_foundation
 import sqflite
 
@@ -18,7 +20,9 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
