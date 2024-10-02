99734728715f92392c386993e99e995698d27d05
aliNassef <anassef798@gmail.com>
2024-10-02 10:02:32 +0300

    fqs ui done

--- lib/features/profile/presentation/views/faqs_view.dart
+++ lib/features/profile/presentation/views/faqs_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:taswaq/core/utils/app_colors.dart';
 
+import '../../../../core/shared/widgets/constants.dart';
+import '../../../../core/shared/widgets/spacers.dart';
 import '../../../../core/utils/app_styles.dart';
 
 class FaqsView extends StatelessWidget {
@@ -9,7 +12,14 @@ class FaqsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildChangePassAppBar(),
-      body: const Center(),
+      body: ListView.separated(
+        padding: EdgeInsets.symmetric(horizontal: kHorizantalpadding),
+        itemBuilder: (cxt, index) {
+          return const FaqsItem();
+        },
+        separatorBuilder: (cxt, index) => const VerticalSpace(16),
+        itemCount: 10,
+      ),
     );
   }
 
@@ -21,6 +31,27 @@ class FaqsView extends StatelessWidget {
       ),
       elevation: 0,
       backgroundColor: Colors.transparent,
+    );
+  }
+}
+
+class FaqsItem extends StatelessWidget {
+  const FaqsItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Can I cancel my order?',
+          style: AppStyles.textStyle14M.copyWith(color: AppColors.blackColor),
+        ),
+        Text(
+          'Yes only if the order is not dispatched yet. You can contact our customer service department to get your order canceled.',
+          style: AppStyles.textStyle14R.copyWith(color: AppColors.gray150Color),
+        ),
+      ],
     );
   }
 }
